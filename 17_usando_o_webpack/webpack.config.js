80fb64d14cebebe3656858f0ca6967f6295ce7ed
Eduardo Luiz Zanotto <91149760+Eduardo-Luizz@users.noreply.github.com>
2023-05-31 10:31:41 -0300

    Aprendendo sobre plugins no webpack

--- 17_usando_o_webpack/webpack.config.js
+++ 17_usando_o_webpack/webpack.config.js
@@ -1,17 +1,24 @@
-// const path = require('path')
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: {
         index: './src/index.js',
         // hello: './src/hello.js'
     },
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
     mode: 'development',
     module: {
         rules: [{
             test: /\.css$/,
-            use: ['style-loader', 'css-loader']
+            use: [MiniCssExtractPlugin.loader, 'css-loader']
         }]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ]
     // output: {
     //     path: path.resolve(__dirname, 'public'),
     //     filename: '[name].bundle.min.js'
